b25596c8029a18fd189a3049331743b04e13a991
Heimgartner  Daniel <daniehei@ethz.ch>
2022-03-19 13:15:09 +0100

    init

---
+++ R/hello_world.R
@@ -0,0 +1,12 @@
+#' Hello World
+#'
+#' Add your Name to the print statement.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' hello_world()
+hello_world <- function() {
+  print("Hello from Daniel")
+}
